c7da09dfae52021e59140bc932449ed7113e7831
Manfred Moser <manfred@simpligility.com>
2020-01-27 12:30:20 -0800

    Add some tpch queries

--- tpch/tpch-queries.sql
+++ tpch/tpch-queries.sql
@@ -1,2 +1,37 @@
-USE tpch.sf1;
+USE tpch.tiny;
+
 SELECT name FROM nation;
+
+SELECT
+    (SELECT name FROM region r WHERE regionkey = n.regionkey)
+        AS region_name,
+    n.name AS nation_name,
+    sum(totalprice) orders_sum
+FROM nation n, orders o, customer c
+WHERE n.nationkey = c.nationkey
+  AND c.custkey = o.custkey
+GROUP BY n.nationkey, regionkey, n.name
+ORDER BY orders_sum DESC
+LIMIT 5;
+
+SELECT DISTINCT o.orderkey
+FROM lineitem l
+  JOIN orders o ON o.orderkey = l.orderkey
+  JOIN customer c ON o.custkey = c.custkey
+WHERE c.nationkey IN (                          
+    -- All nations for all suppliers of an item
+    SELECT s.nationkey
+    FROM part p
+      JOIN partsupp ps ON p.partkey = ps.partkey
+      JOIN supplier s ON ps.suppkey = s.suppkey
+    WHERE p.partkey = l.partkey
+);
+
+SELECT
+    n.name AS nation_name,
+    avg(extendedprice) as avg_price
+FROM nation n, orders o, customer c, lineitem l
+WHERE n.nationkey = c.nationkey
+  AND c.custkey = o.custkey
+  AND o.orderkey = l.orderkey
+GROUP BY n.nationkey, n.name;
